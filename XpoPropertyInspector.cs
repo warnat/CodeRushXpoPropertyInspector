84a36a66c7d381ebcafc118aa7964cf0d33391bf
warnat <warnat@gmx.net>
2013-01-09 12:54:54 +0100

    better recognition + vs2012

--- XpoPropertyInspector.cs
+++ XpoPropertyInspector.cs
@@ -10,29 +10,29 @@ using DevExpress.CodeRush.StructuralParser;
 
 namespace CodeIssueSearcher
 {
-	public partial class CodeIssueSearcher : StandardPlugIn
-	{
-		// DXCore-generated code...
-		#region InitializePlugIn
-		public override void InitializePlugIn()
-		{
-			base.InitializePlugIn();
+    public partial class CodeIssueSearcher : StandardPlugIn
+    {
+        // DXCore-generated code...
+        #region InitializePlugIn
+        public override void InitializePlugIn()
+        {
+            base.InitializePlugIn();
 
-			//
-			// TODO: Add your initialization code here.
-			//
-		}
-		#endregion
-		#region FinalizePlugIn
-		public override void FinalizePlugIn()
-		{
-			//
-			// TODO: Add your finalization code here.
-			//
+            //
+            // TODO: Add your initialization code here.
+            //
+        }
+        #endregion
+        #region FinalizePlugIn
+        public override void FinalizePlugIn()
+        {
+            //
+            // TODO: Add your finalization code here.
+            //
 
-			base.FinalizePlugIn();
-		}
-		#endregion
+            base.FinalizePlugIn();
+        }
+        #endregion
 
         private void my_issue_provider_CheckCodeIssues(object sender, CheckCodeIssuesEventArgs ea)
         {
@@ -51,45 +51,34 @@ namespace CodeIssueSearcher
 
                     if (propertyElement.SetMethod != null)
                     {
-
-                        foreach (IElement setterElement in propertyElement.SetMethod.AllChildren)
-                        {
-                            if (setterElement.ElementType == LanguageElementType.MethodCall && setterElement.FullName.Contains("SetPropertyValue"))
-                            {
-                                set_prop_found = true;
-                                IMethodCallStatement call = setterElement as IMethodCallStatement;
-                                if (call != null)
-                                {
-                                    foreach (var item in call.Arguments)
-                                    {
-                                        var i = item as IPrimitiveExpression;
-                                        if (i != null && i.Value.ToString() != element.Name)
-                                            ea.AddError(call.FirstNameRange,
-                                                string.Format("Der Aufruf von SetPropertyValue benutzt nicht den Property-Namen !\r\nBitte \"{0}\" in \"{1}\" ändern !!!",
-                                                i.Value, element.Name));
-                                    }
-                                }
-                            }
-                        }
+                        set_prop_found = ContainsSetProperty(element.Name, propertyElement.SetMethod.AllChildren, ea);
                     }
 
                     bool persistent_found = false;
+                    bool delayed_found = false;
                     if (!set_prop_found)
                     {
                         foreach (object atr in propertyElement.Attributes)
                         {
                             IAttributeElement attr = atr as IAttributeElement;
                             if (attr != null && attr.Name == "Persistent")
-                            {
                                 persistent_found = true;
-                                break;
-                            }
-                        }
-                        if (persistent_found)
-                            ea.AddError(propertyElement.FirstNameRange,
-                                "Die persistente Property braucht SetPropertyValue");
-                    }
 
+                            if (attr != null && attr.Name == "NonPersistent")
+                                persistent_found = false;
+                        
+                            if (attr != null && attr.Name == "Delayed")
+                                delayed_found = true;
+
+                        }
+
+                        if (!persistent_found)
+                            continue;
+
+                        ea.AddError(propertyElement.FirstNameRange, 
+                            "Die persistente Property braucht " +
+                            (delayed_found ? "SetDelayedPropertyValue" : "SetPropertyValue"));
+                    }
                 }
             }
             catch //(Exception ex)
@@ -97,5 +86,43 @@ namespace CodeIssueSearcher
 
             }
         }
-	}
+
+        private static bool ContainsSetProperty(string elementName, IEnumerable<IElement> elements, CheckCodeIssuesEventArgs ea)
+        {
+            if (elements == null) return false;
+            try
+            {
+                foreach(IElement setterElement in elements)
+                {
+                    if (setterElement.ElementType == LanguageElementType.MethodCall
+                                 && (
+                                         setterElement.FullName.Contains("SetPropertyValue")
+                                      || setterElement.FullName.Contains("SetDelayedPropertyValue")
+                                      || setterElement.FullName.Contains("SetPropertyStringWithMaxSize"))
+                                 )
+                    {
+                        IMethodCallStatement call = setterElement as IMethodCallStatement;
+                        if (call != null)
+                        {
+                            foreach (var item in call.Arguments)
+                            {
+                                var i = item as IPrimitiveExpression;
+                                if (i != null && i.Value.ToString() != elementName)
+                                    ea.AddError(call.FirstNameRange,
+                                        string.Format("Der Aufruf von SetPropertyValue benutzt nicht den Property-Namen !\r\nBitte \"{0}\" in \"{1}\" ändern !!!",
+                                        i.Value, elementName));
+                            }
+                        }
+                        return true;
+                    }
+                    if (ContainsSetProperty(elementName, setterElement.AllChildren, ea))
+                        return true;
+                }
+            }
+            catch 
+            {
+            }
+            return false;
+        }
+    }
 }
